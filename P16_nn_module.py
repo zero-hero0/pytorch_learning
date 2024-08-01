6071fd8e57b1bbbcfd9603634218e673be563b45
zero-hero0 <2209978409@qq.com>
2024-08-01 17:12:02 +0800

    add some illustrations

--- P16_nn_module.py
+++ P16_nn_module.py
@@ -5,6 +5,9 @@ class Tudui(nn.Module):
         super().__init__()
 
     def forward(self, input):
+#在 PyTorch 中，forward 方法是一个模型的前向传播函数，用于计算输入数据经过模型后的输出。
+# 在定义一个神经网络模型时，需要继承 torch.nn.Module 类并实现 forward 方法。forward 方法的输入参数通常是张量（Tensor），
+# 表示模型的输入数据。forward 方法应该返回一个张量，表示模型的输出数据
         output = input + 1
         return output
 
