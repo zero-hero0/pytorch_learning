21c6bec2e6d3a8bb2781a76376829b405e2b1a47
zero-hero0 <2209978409@qq.com>
2024-08-01 17:50:39 +0800

    add some illustrations

--- P25_model_pretrained.py
+++ P25_model_pretrained.py
@@ -5,13 +5,15 @@ from torch import nn
 # 数据集过大，无法下载
 
 vgg16_false = torchvision.models.vgg16(pretrained=False)
-vgg16_true = torchvision.models.vgg16(pretrained=True)
+vgg16_true = torchvision.models.vgg16(pretrained=True) #参数训练好的vgg16模型
 
 print(vgg16_true)
 
+# 将全连接层添加到 VGG16 模型的 classifier 部分
 vgg16_true.classifier.add_module('add_linear', nn.Linear(1000, 10))
 print(vgg16_true)
 
 print(vgg16_false)
+# 改变classifier部分的第6层
 vgg16_false.classifier[6] = nn.Linear(4096, 10)
 print(vgg16_false)
