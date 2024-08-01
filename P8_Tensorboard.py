b04ef1f322fb24d2d2153047a154e2959640ee42
zero-hero0 <2209978409@qq.com>
2024-08-01 16:59:12 +0800

    add some illustrations

--- P8_Tensorboard.py
+++ P8_Tensorboard.py
@@ -6,12 +6,13 @@ writer = SummaryWriter("logs")
 image_path = "data/train/bees_image/16838648_415acd9e3f.jpg"
 img_PIL = Image.open(image_path)
 # img_PIL.show()
-img_array = np.array(img_PIL)
+img_array = np.array(img_PIL) # 图片的格式要为numpy数组或者tensor, 所以要改变格式
 print(type(img_array))
 print(img_array.shape)
 
 # writer.add_image("train", img_array, 2, dataformats='HWC')
 writer.add_image("test", img_array, 1, dataformats='HWC')
+#dataformats：一个字符串，用于指定图像数据的格式。在这里，HWC 表示图像数据的格式为高度（H）×宽度（W）×通道（C）。
 
 # y = 2x
 for i in range(100):
